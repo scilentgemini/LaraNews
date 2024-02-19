e0cfb4eadfbddf64b1e0a14c084d5c4235c5c4e7
scilentgemini <rbchitthi@gmail.com>
2024-02-19 23:04:57 +0545

    alpine searchbox implemented - filament panels installed

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,8 +4,10 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
-use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
+use Livewire\Attributes\Computed;
 
 class PostList extends Component
 {
@@ -16,8 +18,28 @@ class PostList extends Component
 
 
     use WithPagination;
+
+    #[Url()]
+    public $sort = 'desc';
+
+    #[Url()]
+    public $search = '';
+
+    public function setSort($sort){
+        $this->sort = ($sort == 'desc') ? 'desc' : 'asc';
+        $this->resetPage();
+    }
+
+
+    #[On('search')]
+    public function updateSearch($search){
+        $this->search = $search;
+    }
+
     #[Computed()]
     public function posts(){
-        return Post::published()->orderBy('published_at', 'desc')->paginate(3);
+        return Post::published()->orderBy('published_at', $this->sort)
+        ->where('title', 'like', "%{$this->search}%")
+        ->paginate(3);
     }
 }
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,9 +1,15 @@
-
 <div class=" px-3 lg:px-7 py-6">
     <div class="flex justify-between items-center border-b border-gray-100">
+        <div class="text-gray-600">
+            @if ($search)
+                Searching: {{ $search }}
+            @endif
+        </div>
         <div class="flex items-center space-x-4 font-light ">
-            <button class="text-gray-500 py-4">Latest</button>
-            <button class="text-gray-900 py-4 border-b border-gray-700">Oldest</button>
+            <button class="{{ $sort === 'desc' ? 'text-gray-900 py-4 border-b border-gray-700' : 'text-gray-500' }} py-4"
+                wire:click="setSort('desc')">Latest</button>
+            <button class="{{ $sort === 'asc' ? 'text-gray-900 py-4 border-b border-gray-700' : 'text-gray-500' }} py-4"
+                wire:click="setSort('asc')">Oldest</button>
         </div>
     </div>
     <div class="py-4">
